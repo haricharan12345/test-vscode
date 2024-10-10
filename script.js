d280c2d5abc3fb3bddf2756e86bf8cb1d06ac0cf
haricharan12345 <gattyharichaan@gmail.com>
2024-10-10 18:22:12 +0530

    modified index.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-const name = "hari";
-console.log(name);
+// const name = "hari";
+// console.log(name);
+const enterName = document.querySelector("name");
+function handleSubmit() {
+  console.log("entered Name", enterName.value);
+}
